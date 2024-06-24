99e58003c28c1f9418d7cf434e6fbc0bb059ec8d
Learning-stud <jayahalpara123456@gmail.com>
2024-06-24 18:16:38 +0530

    ,..,,.,.

--- Inc_42_Data_Scrap/details.md
+++ Inc_42_Data_Scrap/details.md
@@ -1,3 +1,3 @@
   ##   Link => https://inc42.com/buzz/from-zepto-to-bira-91-indian-startups-raised-800-mn-this-week/
 
-  ## Xpath Of the Table  =>  //*[@id="post-463847"]/div[2]/div/div/div[2]/div[3]/div[4]/table
+  ##   => xpath = '//div[@class="funding-table-wrapper"]/table'
--- Inc_42_Data_Scrap/inc.py
+++ Inc_42_Data_Scrap/inc.py
@@ -1,33 +1,46 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# Fetch the webpage
-url = 'https://inc42.com/buzz/from-zepto-to-bira-91-indian-startups-raised-800-mn-this-week/'
-response = requests.get(url)
-html_content = response.content
 
-# Parse HTML
-soup = BeautifulSoup(html_content, 'html.parser')
+url = "https://inc42.com/tag/funding-galore/"
 
-# Find the table
-table = soup.find('tbody  ')
 
-# Extract table data and save to CSV
-if table:
-    with open('startup_data.csv', mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
+driver = webdriver.Chrome()
 
-        # Write header row
-        header_row = [header.get_text(strip=True) for header in table.find_all('th')]
-        writer.writerow(header_row)
 
-        # Write data rows
-        rows = table.find_all('tr')
-        for row in rows[1:]:  # Skip the first row (header row is already written)
-            csv_row = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
-            writer.writerow(csv_row)
+driver.get(url)
 
-    print('CSV file created successfully.')
-else:
-    print('No table found on the webpage.')
+
+links = []
+
+try:
+    while True:
+
+        buttons = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='load-more-feed']")))
+
+        driver.execute_script("arguments[0].click();", buttons)
+
+
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='main']/div/div/div[1]/h2/a")))
+
+
+        elements = driver.find_elements(By.XPATH, "//*[@id='main']/div/div/div[1]/h2/a")
+        for element in elements:
+            link = element.get_attribute("href")
+            links.append(link)
+
+
+        buttons = driver.find_elements(By.XPATH, "//*[@id='load-more-feed']")
+        if not buttons:
+            break
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    driver.quit()
+
+for index, link in enumerate(links, start=1):
+    print(f"Link {index}: {link}")
