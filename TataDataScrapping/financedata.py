76ce9d774b10dcc44592f318cbf2ab22e8723a3a
Learning-stud <jayahalpara123456@gmail.com>
2024-06-25 18:03:47 +0530

    ,..,,.,.

--- TataDataScrapping/financedata.py
+++ TataDataScrapping/financedata.py
@@ -2,36 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# URL of the webpage
 url = "https://guidestarindia.org/Finances.aspx?CCReg=5348"
-
-# Send a GET request to the URL
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
-    # Parse the HTML content
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Find all elements with class 'CPBData'
     elements = soup.find_all(class_="CPBData")
-
-    # Initialize dictionary to store data
     data = {}
 
-    # Extract key-value pairs
     for element in elements:
-        span_tags = element.find_all('span')
+        span = element.find_all('span')
         anchor_tags = element.find_all('a')
-        for i in range(len(span_tags)):
-            # Extract text from span tag
-            key = span_tags[i].text.strip()
+
+
+        for i in range(len(span)):
+            key = span[i].text.strip()
+
             if i < len(anchor_tags) and not anchor_tags[i].has_attr('class'):
-                # Extract URL from anchor tag
                 value = anchor_tags[i]['href']
-            else:
-                value = span_tags[i-1].text.strip()
-            # Remove unwanted symbols
+            else:# to remove unwanted text
+                value = span[i-1].text.strip()
             key = key.replace('\r\n', '').replace('\n', '').replace('\u00a0', '').strip()
             value = value.replace('\r\n', '').replace('\n', '').replace('\u00a0', '').strip()
             data[key] = value
