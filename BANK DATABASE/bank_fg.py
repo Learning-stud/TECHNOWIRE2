3bc34ff56e539367cbd4362e5781fa10e356e253
Learning-stud <jayahalpara123456@gmail.com>
2024-07-10 16:16:55 +0530

    .,.,,.

--- BANK DATABASE/bank_fg.py
+++ BANK DATABASE/bank_fg.py
@@ -251,23 +251,21 @@ from selenium.webdriver.support.ui import Select
 import mysql.connector
 from bs4 import BeautifulSoup
 
-# Function to connect to MySQL database
-def mysql_connection():
-    try:
-        conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='Jy%02kl&R@bh',
-            database='bc_bank'
-        )
-        if conn.is_connected():
-            print('Connected to MySQL database')
-            return conn
-    except mysql.connector.Error as e:
-        print(f"Error connecting to MySQL database: {e}")
-        return None
+# def mysql_connection():
+#     try:
+#         conn = mysql.connector.connect(
+#             host='localhost',
+#             user='root',
+#             password='Jy%02kl&R@bh',
+#             database='bc_bank'
+#         )
+#         if conn.is_connected():
+#             print('Connected to MySQL database')
+#             return conn
+#     except mysql.connector.Error as e:
+#         print(f"Error connecting to MySQL database: {e}")
+#         return None
 
-# Initialize the WebDriver without headless mode
 driver = webdriver.Chrome()
 
 # URL of the page
@@ -276,28 +274,21 @@ url = "https://www.bcregistry.org.in/iba/home/HomeAction.do?doBCPortal=yes"
 driver.get(url)
 
 try:
-    # Wait for the State dropdown to be interactive
     state_dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="stateId"]')))
     Select(state_dropdown).select_by_value('10')  # Select Gujarat
 
-    # Wait for the District dropdown to be interactive
     district_dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="districtId"]')))
-    Select(district_dropdown).select_by_index(2)  # Select Ahmedabad
+    Select(district_dropdown).select_by_index(2)
 
-    # Click the Go button
     go_button = driver.find_element(By.XPATH, '//*[@id="nav-main1"]/ul/div/div/div[3]/div/div/a')
     go_button.click()
 
-    # Wait for the table to load
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, 'table')))
 
-    # Parse the HTML with BeautifulSoup
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # Find the table containing the data
     table = soup.find('table', class_='table table-hover table-light')
 
-    # Connect to MySQL database
     conn = mysql_connection()
     if not conn:
         driver.quit()
@@ -306,85 +297,73 @@ try:
     cursor = conn.cursor()
 
     try:
-        # Find all rows in the table
         rows = table.find_all('tr')
 
         for row in rows:
-            # Extract data from each row
             columns = row.find_all('td')
             if columns:
-                # Extract specific data
                 bc_name = columns[1].text.strip()
                 mobile_no = columns[2].text.strip()
                 pincode = columns[3].text.strip()
                 bank_name = columns[4].text.strip()
 
-                # Extract href attribute from the anchor tag within bc_name
                 href = columns[1].find('a')['href'] if columns[1].find('a') else None
 
-                # Insert data into MySQL table
-                insert_query = "INSERT INTO bc_details (bc_name, mobile_no, pincode, bank_name, href) VALUES (%s, %s, %s, %s, %s)"
-                data = (bc_name, mobile_no, pincode, bank_name, href)
-                cursor.execute(insert_query, data)
+                # insert_query = "INSERT INTO bc_details (bc_name, mobile_no, pincode, bank_name, href) VALUES (%s, %s, %s, %s, %s)"
+                # data = (bc_name, mobile_no, pincode, bank_name, href)
+                # cursor.execute(insert_query, data)
                 conn.commit()
 
-                print(f"Inserted: {bc_name}, {mobile_no}, {pincode}, {bank_name}, {href}")
+    #             print(f"Inserted: {bc_name}, {mobile_no}, {pincode}, {bank_name}, {href}")
 
     except Exception as e:
         print(f"Error occurred during data insertion: {str(e)}")
 
     finally:
-        # Close cursor and connection
         cursor.close()
         conn.close()
 
-    # Handle Captcha
-    # Wait for modal to appear
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'modal-body')))
 
-    # Find captcha text element
     captcha_element = driver.find_element(By.CLASS_NAME, 'captcha_text')
     captcha_text = captcha_element.text.strip()
 
-    # Input captcha text into the input field
     captcha_input = driver.find_element(By.ID, 'cap_search')
-    captcha_input.clear()  # Clear existing input
+    captcha_input.clear()
     captcha_input.send_keys(captcha_text)
 
-    # Click the verify button
     verify_button = driver.find_element(By.CLASS_NAME, 'btn.blue')
     verify_button.click()
 
-    # Wait for a while (for demonstration purposes)
-    time.sleep(5)  # Wait for 5 seconds
+
+    time.sleep(5)
 
 except Exception as e:
     print(f"Error occurred: {str(e)}")
 
 finally:
-    # Close the browser
     driver.quit()
 
 # Function to fetch and print data from MySQL
-def fetch_data_from_mysql():
-    conn = mysql_connection()
-    if not conn:
-        return
+# def fetch_data_from_mysql():
+#     conn = mysql_connection()
+#     if not conn:
+#         return
 
-    cursor = conn.cursor()
-    try:
-        cursor.execute("SELECT * FROM bc_details")
-        rows = cursor.fetchall()
+#     cursor = conn.cursor()
+#     try:
+#         cursor.execute("SELECT * FROM bc_details")
+#         rows = cursor.fetchall()
 
-        for row in rows:
-            print(row)
+#         for row in rows:
+#             print(row)
 
-    except mysql.connector.Error as e:
-        print(f"Error fetching data: {e}")
+#     except mysql.connector.Error as e:
+#         print(f"Error fetching data: {e}")
 
-    finally:
-        cursor.close()
-        conn.close()
+#     finally:
+#         cursor.close()
+#         conn.close()
 
-# Fetch and print the data from MySQL database
-fetch_data_from_mysql()
+# # Fetch and print the data from MySQL database
+# fetch_data_from_mysql()
---
+++ TO CSV FILE/toCsv.py
@@ -0,0 +1,83 @@
+import time
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+
+# Initialize the WebDriver
+driver = webdriver.Chrome()
+
+# URL of the page
+url = "https://www.bcregistry.org.in/iba/home/HomeAction.do?doBCPortal=yes"
+
+driver.get(url)
+
+try:
+    # Wait for the State dropdown to be clickable and select Gujarat
+    state_dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="stateId"]')))
+    Select(state_dropdown).select_by_value('12')
+
+    district_dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="districtId"]')))
+    Select(district_dropdown).select_by_index(38)
+
+
+    go_button = driver.find_element(By.XPATH, '//*[@id="nav-main1"]/ul/div/div/div[3]/div/div/a')
+    go_button.click()
+
+
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, 'table')))
+
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+
+    table = soup.find('table', class_='table table-hover table-light')
+
+
+    with open('Vapi-Gujarat.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Write the header row
+        writer.writerow(['bc_name', 'mobile_no', 'pincode', 'bank_name'])
+
+        try:
+            rows = table.find_all('tr')
+
+            for row in rows:
+                columns = row.find_all('td')
+                if columns:
+                    bc_name = columns[1].text.strip()
+                    mobile_no = columns[2].text.strip()
+                    pincode = columns[3].text.strip()
+                    bank_name = columns[4].text.strip()
+
+                    href = columns[1].find('a')['href'] if columns[1].find('a') else None
+
+                    writer.writerow([bc_name, mobile_no, pincode, bank_name])
+
+                    print(f"{bc_name}, {mobile_no}, {pincode}, {bank_name}")
+
+        except Exception as e:
+            print(f"Error occurred during data extraction: {str(e)}")
+
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'modal-body')))
+
+    captcha_element = driver.find_element(By.CLASS_NAME, 'captcha_text')
+    captcha_text = captcha_element.text.strip()
+
+    captcha_input = driver.find_element(By.ID, 'cap_search')
+    captcha_input.clear()
+    captcha_input.send_keys(captcha_text)
+
+    verify_button = driver.find_element(By.CLASS_NAME, 'btn.blue')
+    verify_button.click()
+
+    time.sleep(10)
+
+except Exception as e:
+    print(f"Error occurred: {str(e)}")
+
+finally:
+    driver.quit()
